f934f9601268848cd31aa6ea8142b5ffb5d171e3
Smith Braz <106747435+thiagosmith@users.noreply.github.com>
2025-01-19 12:17:46 -0300

    Update script11.sh

--- script11.sh
+++ script11.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #atividade de selecao 1 - inicio de pentest
-echo "O CLIENTE AUTORIZOU O PENTEST?"
+echo "Ja tem cerveja?"
 echo "1 - SIM"
 echo "2 - NAO"
 read resp
 case $resp in
 "1")
-	echo "PODE INICIAR O PENTEST"
+	echo "PODE INICIAR A BEBEDEIRA"
 ;;
 "2")
-	echo "PENTEST PENDENTE. AGUARDE O CLIENTE AUTORIZAR!!!"
+	echo "ASSIM NAO DA PRA BEBER. LEVANTA E VAI BUSCAR!!!"
 ;;
 esac
